53a306dc7bc2bbb33d6cac773e4ed2cb05e7402f
uiaaaa <s6804062612013@email.kmutnb.ac.th>
2026-07-01 12:04:40 +0700

    HOMEWORK DS

--- what array.cpp
+++ what array.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
 #define MAXSIZE 100
+#defind DELETE -1
 
 // Global variables
 int arr[MAXSIZE];
-int n=0;    // to store the number of elements in arr 
+int n=0;    // to store the number of elements in arr []
 
 // Function DECLARATION
 void insert_at_the_begining(int x);
 void insert_at_the_end(int x);
 void insert_at_position(int x,int pos);
 void print_arr();
+
+// homework
+void delete_from_begining();
+void delete_from_end();
+void delete_from_position();
 
 
 int main() {
@@ -46,7 +52,7 @@ void insert_at_the_end(int x) {
 }
 
 void insert_at_position(int x,int pos) {
-	// make room at [pos] by moving arr[0] to the last element
+	// make room at [pos] by moving arr[pos] to the last element
 	// to the last element arr[n]
 	arr[n] = arr[pos];
 	arr[pos] = x;
